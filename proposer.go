e9d32eb93da22af6df018bd43386c24cc14e9f5a
Kent Kawashima <kent@Kents-MacBook-Pro.local>
2017-08-11 04:37:20 +0900

    Adds proposer.go

---
+++ proposer.go
@@ -0,0 +1,60 @@
+package abcgo
+
+import (
+	"math"
+
+	"github.com/atgjack/prob"
+)
+
+// Proposer generates parameter values randomly.
+type Proposer interface {
+	Moments() []float64
+	Propose() float64
+	Prob(...float64) []float64
+	LogProb(...float64) []float64
+}
+
+type NormalProposer struct {
+	prob.Normal
+}
+
+func (n *NormalProposer) Moments(names ...string) []float64 {
+	var moments []float64
+	for _, name := range names {
+		switch name {
+		case "Mean":
+			moments = append(moments, n.Mean())
+		case "Variance":
+			moments = append(moments, n.Variance())
+		case "Skewness":
+			moments = append(moments, n.Skewness())
+		case "Kurtosis":
+			moments = append(moments, n.Kurtosis())
+		case "StdDev":
+			moments = append(moments, n.StdDev())
+		case "RelStdDev":
+			moments = append(moments, n.RelStdDev())
+		}
+	}
+	return moments
+}
+
+func (n *NormalProposer) Propose() float64 {
+	return n.Random()
+}
+
+func (n *NormalProposer) Prob(values ...float64) []float64 {
+	res := make([]float64, len(values))
+	for i, v := range values {
+		res[i] = n.Pdf(v)
+	}
+	return res
+}
+
+func (n *NormalProposer) LogProb(values ...float64) []float64 {
+	res := make([]float64, len(values))
+	for i, v := range values {
+		res[i] = math.Log(n.Pdf(v))
+	}
+	return res
+}
