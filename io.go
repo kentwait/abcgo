ecae6e8d8253656b92153563cc86d472f70b2bc3
Kent Kawashima <kent@Kents-MacBook-Pro.local>
2017-08-11 19:16:21 +0900

    Updates docs

--- io.go
+++ io.go
@@ -6,7 +6,13 @@ import (
 	"strconv"
 )
 
-func ToDelimited(samples [][]float64, delimiter, path string) error {
+func FromDelimited(path, delimiter string) [][]float64 {
+
+}
+
+// ToDelimited writes a float64 2D slice to disk as a text file delimited
+// by the given delimiter string.
+func ToDelimited(samples [][]float64, path, delimiter string) error {
 	var buff bytes.Buffer
 	for i := 0; i < len(samples); i++ {
 		for j := 0; j < len(samples[i])-1; j++ {
@@ -18,15 +24,18 @@ func ToDelimited(samples [][]float64, delimiter, path string) error {
 	if err != nil {
 		return err
 	}
+	defer f.Close()
 	buff.WriteTo(f)
 
 	return nil
 }
 
+// ToTab writes a float64 2D slice to disk as a tab-delimited text file.
 func ToTab(samples [][]float64, path string) error {
 	return ToDelimited(samples, "\t", path)
 }
 
+// ToCsv writes a float64 2D slice to disk as a CSV file.
 func ToCsv(samples [][]float64, path string) error {
 	return ToDelimited(samples, ",", path)
 }
