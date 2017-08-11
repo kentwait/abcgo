c028fa3c37989727cd5de4e381cf306777a06309
Kent Kawashima <kent@Kents-MacBook-Pro.local>
2017-08-11 18:38:18 +0900

    Adds LogNormalProposer

--- proposer.go
+++ proposer.go
@@ -265,6 +265,90 @@ func (n *ExponentialProposer) UpdateFields(values ...float64) {
 	n.Lambda = values[0]
 }
 
+// LogNormalProposer is a Proposer based on a normal distribution with a mean
+// mu and standard deviation sigma.
+type LogNormalProposer struct {
+	prob.LogNormal
+}
+
+// NewLogNormalProposer is a constructor that returns a pointer to
+// a new LogNormalProposer.
+func NewLogNormalProposer(mu, sigma float64) *LogNormalProposer {
+	p, _ := prob.NewLogNormal(mu, sigma)
+	return &LogNormalProposer{p}
+}
+
+// Moments returns the statistics of the shape of the lognormal probability
+// density.
+func (n *LogNormalProposer) Moments(names ...string) []float64 {
+	var moments []float64
+	for _, name := range names {
+		switch name {
+		case "Mean":
+			moments = append(moments, n.Mean())
+		case "Variance":
+			moments = append(moments, n.Variance())
+		case "Skewness":
+			moments = append(moments, n.Skewness())
+		case "Kurtosis":
+			moments = append(moments, n.Kurtosis())
+		case "StdDev":
+			moments = append(moments, n.StdDev())
+		case "RelStdDev":
+			moments = append(moments, n.RelStdDev())
+		}
+	}
+	return moments
+}
+
+// Propose returns a random value from the initialized lognormal probability
+// density.
+func (n *LogNormalProposer) Propose() float64 {
+	return n.Random()
+}
+
+// Prob returns the probability of a value in the initialized lognormal
+// probability density.
+func (n *LogNormalProposer) Prob(value float64) float64 {
+	return n.Pdf(value)
+}
+
+// Probs returns probabilities of one or more given values based on the
+// initialized lognormal probability density.
+func (n *LogNormalProposer) Probs(values ...float64) []float64 {
+	res := make([]float64, len(values))
+	for i, v := range values {
+		res[i] = n.Pdf(v)
+	}
+	return res
+}
+
+// LogProb returns the log probability of a value in the initialized lognormal
+// probability density.
+func (n *LogNormalProposer) LogProb(value float64) float64 {
+	return math.Log(n.Pdf(value))
+}
+
+// LogProbs returns the log probabilities of one or more given values based on
+// the initialized lognormal probability density.
+func (n *LogNormalProposer) LogProbs(values ...float64) []float64 {
+	res := make([]float64, len(values))
+	for i, v := range values {
+		res[i] = math.Log(n.Pdf(v))
+	}
+	return res
+}
+
+// UpdateFields updates the mean and standard deviation of
+// the probability density of the normal distribution depending on the number
+// of values given.
+func (n *LogNormalProposer) UpdateFields(values ...float64) {
+	n.Mu = values[0]
+	if len(values) == 2 {
+		n.Sigma = values[1]
+	}
+}
+
 // Proposers is a list of Proposer types to accomodate proposing new
 // values to multiple variables.
 type Proposers []Proposer
