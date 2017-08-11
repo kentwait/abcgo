066da19e48aa00c751f1febe558512251564e500
Kent Kawashima <kent@Kents-MacBook-Pro.local>
2017-08-11 18:05:57 +0900

    Updates docs

--- proposer.go
+++ proposer.go
@@ -17,14 +17,21 @@ type Proposer interface {
 	UpdateMoments(...float64)
 }
 
+// NormalProposer is a Proposer based on a normal distribution with a mean
+// mu and standard deviation sigma.
 type NormalProposer struct {
 	prob.Normal
 }
 
+// NewNormalProposer is a constructor that returns a pointer to
+// a new NormalProposer.
 func NewNormalProposer(mu, sigma float64) *NormalProposer {
-	return &NormalProposer{prob.Normal{mu, sigma}}
+	n, _ := prob.NewNormal(mu, sigma)
+	return &NormalProposer{n}
 }
 
+// Moments returns the statistics of the shape of the normal distribution
+// proposer.
 func (n *NormalProposer) Moments(names ...string) []float64 {
 	var moments []float64
 	for _, name := range names {
@@ -46,14 +53,19 @@ func (n *NormalProposer) Moments(names ...string) []float64 {
 	return moments
 }
 
+// Propose returns a random value from the initialized normal distribution.
 func (n *NormalProposer) Propose() float64 {
 	return n.Random()
 }
 
+// Prob returns the probability of a value in the initialized normal
+// distribution.
 func (n *NormalProposer) Prob(value float64) float64 {
 	return n.Pdf(value)
 }
 
+// Probs returns probabilities of one or more given values based on the
+// initialized normal distribution.
 func (n *NormalProposer) Probs(values ...float64) []float64 {
 	res := make([]float64, len(values))
 	for i, v := range values {
@@ -62,10 +74,14 @@ func (n *NormalProposer) Probs(values ...float64) []float64 {
 	return res
 }
 
+// LogProb returns the log probability of a value in the initialized normal
+// distribution.
 func (n *NormalProposer) LogProb(value float64) float64 {
 	return math.Log(n.Pdf(value))
 }
 
+// LogProbs returns the log probabilities of one or more given values based on
+// the initialized normal distribution.
 func (n *NormalProposer) LogProbs(values ...float64) []float64 {
 	res := make([]float64, len(values))
 	for i, v := range values {
@@ -74,6 +90,9 @@ func (n *NormalProposer) LogProbs(values ...float64) []float64 {
 	return res
 }
 
+// UpdateMoments updates the mean, and standard deviation of
+// the probability density of the normal distribution depending on the number
+// of values given. If only one value is given,
 func (n *NormalProposer) UpdateMoments(moments ...float64) {
 	n.Mu = moments[0]
 	if len(moments) == 2 {
@@ -81,8 +100,12 @@ func (n *NormalProposer) UpdateMoments(moments ...float64) {
 	}
 }
 
+// Proposers is a list of Proposer types to accomodate proposing new
+// values to multiple variables.
 type Proposers []Proposer
 
+// Propose gets new random values from each Proposer using their
+// Propose() method.
 func (p Proposers) Propose() []float64 {
 	params := make([]float64, len(p))
 	for i, proposer := range p {
@@ -91,6 +114,8 @@ func (p Proposers) Propose() []float64 {
 	return params
 }
 
+// Probs returns the probability of each value based on the probability
+// density of its corresponding Proposer by position.
 func (p Proposers) Probs(values ...float64) []float64 {
 	probs := make([]float64, len(values))
 	for i, v := range values {
@@ -99,6 +124,8 @@ func (p Proposers) Probs(values ...float64) []float64 {
 	return probs
 }
 
+// LogProbs returns the log probability of each value based on the probability
+// density of its corresponding Proposer by position.
 func (p Proposers) LogProbs(values ...float64) []float64 {
 	probs := make([]float64, len(values))
 	for i, v := range values {
@@ -107,6 +134,8 @@ func (p Proposers) LogProbs(values ...float64) []float64 {
 	return probs
 }
 
+// TotalProb returns the total probability of the given values measured based
+// on it corresponding Proposer.
 func (p Proposers) TotalProb(values ...float64) float64 {
 	prob := float64(1)
 	for i, v := range values {
@@ -115,6 +144,8 @@ func (p Proposers) TotalProb(values ...float64) float64 {
 	return prob
 }
 
+// TotalLogProb returns the total log probability of the given values measured
+// based on it corresponding Proposer.
 func (p Proposers) TotalLogProb(values ...float64) float64 {
 	prob := float64(0)
 	for i, v := range values {
